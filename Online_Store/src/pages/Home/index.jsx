b430962ec164d773438fc3fb3825e2b27291b383
Ernestoc14 <descreed34@hotmail.com>
2023-06-12 23:58:22 -0500

    Calling the Products API

--- Online_Store/src/pages/Home/index.jsx
+++ Online_Store/src/pages/Home/index.jsx
@@ -1,7 +1,14 @@
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card"
+import { useState, useEffect } from "react"
 
 function Home() {
+	const [items, setItems] = useState(null)
+	
+	//Call to API 
+	useEffect(() => {
+		fetch('https://api.escuelajs.co/api/v1/products').then(response => console.log(response.json()))
+	}, [])
 	return (
 		<Layout>
 			<h1>Exclusive Products</h1>

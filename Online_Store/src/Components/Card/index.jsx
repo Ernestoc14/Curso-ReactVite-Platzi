449a7a537be5fdd0f011159f13f82875852116ad
Ernestoc14 <descreed34@hotmail.com>
2023-06-14 23:52:33 -0500

    Setting the Items Shopping Cart Counter

--- Online_Store/src/Components/Card/index.jsx
+++ Online_Store/src/Components/Card/index.jsx
@@ -1,8 +1,19 @@
+import { ShoppingCartContext } from "../../Context"
+import { useContext } from "react"
+
 const Card = ({category, title, price, image}) => {
+    const context = useContext(ShoppingCartContext)
+    
     return (
         <div className='bg-white text-black cursor-pointer w-56 h-60 rounded-lg '>
             <figure className='relative mb-2 w-full h-4/5'>
-                <button className="absolute top-0 right-0 p-1 m-2 w-6 h-6 flex justify-center items-center bg-white rounded-full">+</button>
+                <button 
+                    className="absolute top-0 right-0 p-1 m-2 w-6 h-6 
+                    flex justify-center items-center bg-white rounded-full"
+                    onClick={() => context.setItemsCounter(context.itemsCounter+1)}
+                >
+                    +
+                </button>
                 <img className="w-full h-full object-cover rounded-lg" src={image} alt="" />
                 <span className="absolute bottom-0 left-0 bg-slate-200 rounded-lg text-black text-xs m-2 px-3 py-0.5">{category}</span>
             </figure>
--- Online_Store/src/Context/index.jsx
+++ Online_Store/src/Context/index.jsx
@@ -1,9 +1,16 @@
-import { createContext } from "react"
+import { createContext, useState } from "react"
 
-const ShoppingCartContext = createContext()
+export const ShoppingCartContext = createContext()
 export const ShoppingCartProvider = ({children}) => {
+    const [itemsCounter, setItemsCounter] = useState(0)
+    console.log(itemsCounter)
+
     return(
-        <ShoppingCartContext.Provider>
+        <ShoppingCartContext.Provider 
+            value={{
+                itemsCounter,
+                setItemsCounter
+            }}>
             {children}
         </ShoppingCartContext.Provider>
     )
